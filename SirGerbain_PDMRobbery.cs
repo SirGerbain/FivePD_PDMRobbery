247fe9c193656bee429b530a70fb42caba284360
BuildTools <unconfigured@null.spigotmc.org>
2023-01-17 22:34:07 +0100

    Fixes and improvements

--- SirGerbain_PDMRobbery.cs
+++ SirGerbain_PDMRobbery.cs
@@ -17,13 +17,16 @@ namespace SirGerbain_PDMRobbery
     public class SirGerbain_PDMRobbery : FivePD.API.Callout
     {
 
-        Ped robber;
+        Ped robber, swat;
         List<VehicleHash> carList = new List<VehicleHash>();
         List<PedHash> robberList = new List<PedHash>(); 
         Vector3 robberyLocation;
         Random random = new Random();
         Vehicle robbedVehicle;
         bool arrivalOnScene = false;
+        bool chaseStarted = false;
+        bool air1Dispatched = false;
+        bool air1HasSwat = false;
 
         public SirGerbain_PDMRobbery()
         {
@@ -40,7 +43,7 @@ namespace SirGerbain_PDMRobbery
             ShortName = "PDM Robbery in progress";
             CalloutDescription = "PDM Robbery";
             ResponseCode = 3;
-            StartDistance = 150f;
+            StartDistance = 200f;
 
         }
 
@@ -51,7 +54,7 @@ namespace SirGerbain_PDMRobbery
 
             PlayerData playerData = Utilities.GetPlayerData();
             string displayName = playerData.DisplayName;
-            Notify("~r~[PDM Robbery] ~y~Officer ~b~" + displayName + ",~y~ someone is tampering with a vehicle in the PDM showroom!");
+            Notify("~r~[PDM Robbery] ~y~Officer ~b~" + displayName + ",~y~ someone is tampering with a vehicle at PDM!");
 
         }
 
@@ -62,13 +65,77 @@ namespace SirGerbain_PDMRobbery
 
             while (!arrivalOnScene)
             {
-                await BaseScript.Delay(1000);
+                await BaseScript.Delay(500);
                 float distance = Game.PlayerPed.Position.DistanceToSquared(robberyLocation);
                 if (distance < 200f)
                 {
-                    robber.Task.CruiseWithVehicle(robbedVehicle, 150f, 786988); ;//786988
+                    robber.Task.CruiseWithVehicle(robbedVehicle, 150f, 786988);
 
                     arrivalOnScene = true;
+                    break;
+                }
+            }
+
+            while (!chaseStarted && arrivalOnScene)
+            {
+                await BaseScript.Delay(1000);
+                float distance = Game.PlayerPed.Position.DistanceToSquared(robber.Position);
+                if (distance < 250f)
+                {
+                    robber.Task.ClearAll();
+                    robber.Task.FleeFrom(closest);
+                    if (random.Next(0, 100) < 20)
+                    {
+                        robber.Task.VehicleShootAtPed(closest);
+                        await BaseScript.Delay(random.Next(1000, 5000));
+                        robber.Task.ClearAll();
+                        robber.Task.CruiseWithVehicle(robbedVehicle, 150f, 786988);
+                    }
+                }
+                else
+                {
+                    robber.Task.ClearAll();
+                    robber.Task.CruiseWithVehicle(robbedVehicle, 150f, 786988);
+                }
+
+                if(!air1Dispatched && (random.Next(0, 100) < 15))
+                {
+                    air1Dispatched = true;
+                    Notify("We called in Air-1 for you.");
+
+                    Vehicle helicopter = await SpawnVehicle(VehicleHash.Polmav, new Vector3(robbedVehicle.Position.X, robbedVehicle.Position.Y, 200));
+                    helicopter.CurrentRPM = 5000;
+                    Ped pilot = await SpawnPed(PedHash.Prisguard01SMM, new Vector3(robbedVehicle.Position.X, robbedVehicle.Position.Y, 200));
+                    pilot.SetIntoVehicle(helicopter, VehicleSeat.Driver);
+                    pilot.AlwaysKeepTask = true;
+                    pilot.BlockPermanentEvents = true;
+                    pilot.Task.ChaseWithHelicopter(robbedVehicle, new Vector3(35f, 35f, 35f));
+
+                    if(random.Next(0, 100) < 25)
+                    {
+                        swat = await SpawnPed(PedHash.Swat01SMY, new Vector3(robbedVehicle.Position.X, robbedVehicle.Position.Y, 200));
+                        swat.SetIntoVehicle(helicopter, VehicleSeat.LeftRear);
+                        swat.AlwaysKeepTask = true;
+                        swat.BlockPermanentEvents = true;
+                        swat.Weapons.Give(WeaponHash.AssaultSMG, 250, true, true);
+                        air1HasSwat = true;
+                    }
+
+                }
+
+                if (air1HasSwat)
+                {
+                    swat.Task.VehicleShootAtPed(robber);
+                    await BaseScript.Delay(random.Next(1000, 5000));
+                    swat.Task.ClearAll();
+                }
+
+                if (robbedVehicle.IsEngineOnFire)
+                {
+                    robber.Task.ClearAll();
+                    robber.Task.FightAgainst(closest);
+
+                    chaseStarted = true;
                     break;
                 }
             }
@@ -90,6 +157,7 @@ namespace SirGerbain_PDMRobbery
             robber.Weapons.Give(WeaponHash.Pistol, 250, true, true);
 
             robber.SetIntoVehicle(robbedVehicle, VehicleSeat.Driver);
+            robber.Task.CruiseWithVehicle(robbedVehicle, 150f, 786988);
 
             PedData data = new PedData();
             List<Item> items = new List<Item>();
@@ -118,7 +186,7 @@ namespace SirGerbain_PDMRobbery
         }
         private void Notify(string message)
         {
-            ShowNetworkedNotification(message, "CHAR_CALL911", "CHAR_CALL911", "Dispatch", "Prisoner Transport", 15f);
+            ShowNetworkedNotification(message, "CHAR_CALL911", "CHAR_CALL911", "Dispatch", "AIR-1", 15f);
         }
         private void DrawSubtitle(string message, int duration)
         {
